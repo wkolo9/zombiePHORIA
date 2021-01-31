d1ad871e36bf13dbc260f1239d53205490e716bc
wkolo9 <77991595+wkolo9@users.noreply.github.com>
2021-01-31 19:44:51 +0100

    Update zombiephoria.sql

--- client/zombiephoria.sql
+++ client/zombiephoria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 05 Sty 2021, 16:28
+-- Czas generowania: 26 Sty 2021, 14:13
 -- Wersja serwera: 10.4.17-MariaDB
 -- Wersja PHP: 8.0.0
 
@@ -29,33 +29,44 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `uzytkownicy` (
   `id` int(11) NOT NULL,
+  `user` text COLLATE utf8_polish_ci NOT NULL,
   `login` text COLLATE utf8_polish_ci NOT NULL,
   `pass` text COLLATE utf8_polish_ci NOT NULL,
   `email` text COLLATE utf8_polish_ci NOT NULL,
   `lvl` int(11) NOT NULL,
   `currency` int(11) NOT NULL,
+  `lvl_glock` int(11) NOT NULL,
   `lvl_sniper` int(11) NOT NULL,
+  `lvl_awp` int(11) NOT NULL,
   `lvl_heavy` int(11) NOT NULL,
-  `user` text COLLATE utf8_polish_ci NOT NULL
+  `lvl_minigun` int(11) NOT NULL,
+  `lvl_speedy` int(11) NOT NULL,
+  `lvl_smg` int(11) NOT NULL,
+  `xp` int(11) NOT NULL,
+  `devotion_lvl` int(11) NOT NULL,
+  `chosen_class` int(11) NOT NULL,
+  `logged_in` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Zrzut danych tabeli `uzytkownicy`
 --
 
-INSERT INTO `uzytkownicy` (`id`, `login`, `pass`, `email`, `lvl`, `currency`, `lvl_sniper`, `lvl_heavy`, `user`) VALUES
-(1, 'adam', 'qwerty', 'adam@gmail.com', 213, 5675, 342, 0, 'WoXuS'),
-(2, 'marek', 'asdfg', 'marek@gmail.com', 324, 1123, 4325, 15, 'maro13'),
-(3, 'anna', 'zxcvb', 'anna@gmail.com', 4536, 17, 120, 25, 'anka'),
-(4, 'andrzej', 'asdfg', 'andrzej@gmail.com', 5465, 132, 189, 0, 'andrewGolara'),
-(5, 'justyna', 'yuiop', 'justyna@gmail.com', 245, 890, 554, 0, 'justyś15'),
-(6, 'kasia', 'hjkkl', 'kasia@gmail.com', 267, 980, 109, 12, 'kasiox'),
-(7, 'beata', 'fgthj', 'beata@gmail.com', 565, 356, 447, 77, 'beti69'),
-(8, 'jakub', 'ertyu', 'jakub@gmail.com', 2467, 557, 876, 0, 'jacob420'),
-(9, 'janusz', 'cvbnm', 'janusz@gmail.com', 65, 456, 2467, 0, 'marofrajer'),
-(10, 'roman', 'dfghj', 'roman@gmail.com', 97, 226, 245, 23, 'romekxd'),
-(16, 'najek13xx', '12345678', 'najman.marcin@gmail.com', 1, 0, 0, 0, 'Najman'),
-(17, 'krzysiek13', 'opensource', 'krzysiek@podlesie.pl', 1, 0, 0, 0, 'krzykox');
+INSERT INTO `uzytkownicy` (`id`, `user`, `login`, `pass`, `email`, `lvl`, `currency`, `lvl_glock`, `lvl_sniper`, `lvl_awp`, `lvl_heavy`, `lvl_minigun`, `lvl_speedy`, `lvl_smg`, `xp`, `devotion_lvl`, `chosen_class`, `logged_in`) VALUES
+(1, 'WoXuS', 'adam', 'qwerty', 'adam@gmail.com', 0, 0, 0, 13, 10, 2, 2, 25, 1, 37, 1, 0, 1),
+(2, 'maro13', 'marek', 'asdfg', 'marek@gmail.com', 24, 1100, 0, 2, 0, 15, 0, 0, 0, 1, 3, 0, 0),
+(3, 'anka', 'anna', 'zxcvb', 'anna@gmail.com', 36, 100, 0, 1, 0, 5, 0, 0, 0, 1, 2, 0, 0),
+(4, 'andrewGolara', 'andrzej', 'asdfg', 'andrzej@gmail.com', 65, 200, 0, 1, 0, 0, 0, 0, 0, 1, 2, 0, 0),
+(5, 'justyś15', 'justyna', 'yuiop', 'justyna@gmail.com', 45, 900, 0, 5, 0, 0, 0, 0, 0, 1, 5, 0, 0),
+(6, 'kasiox', 'kasia', 'hjkkl', 'kasia@gmail.com', 26, 900, 0, 9, 0, 12, 0, 0, 0, 1, 7, 0, 0),
+(7, 'beti69', 'beata', 'fgthj', 'beata@gmail.com', 55, 300, 0, 4, 0, 7, 0, 0, 0, 1, 2, 0, 0),
+(8, 'jacob420', 'jakub', 'ertyu', 'jakub@gmail.com', 24, 500, 0, 8, 0, 0, 0, 0, 0, 1, 3, 0, 0),
+(9, 'marofrajer', 'janusz', 'cvbnm', 'janusz@gmail.com', 65, 400, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0),
+(10, 'romekxd', 'roman', 'dfghj', 'roman@gmail.com', 97, 200, 0, 4, 0, 23, 0, 0, 0, 1, 0, 0, 0),
+(16, 'Najman', 'najek13xx', '12345678', 'najman.marcin@gmail.com', 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
+(20, 'nickname', 'login', 'haslo12345', 'email@gmail.com', 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
+(26, 'przyklad', 'przykladowylogin', '12345678', 'mail@gmail.com', 0, 20000, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0),
+(27, 'xdxdxdxd', '12345678', '12345678', 'galeris1233@interia.pl', 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -76,7 +87,7 @@ ALTER TABLE `uzytkownicy`
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
